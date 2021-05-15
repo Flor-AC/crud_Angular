466e0989290e9186fec0444f347f58a391610544
Flor-AC <florac206@gmail.com>
2021-05-14 19:52:17 -0500

    Se agregaron los botones listar y nuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crud-angular';
+
+  constructor(private router: Router){}
+
+  Nuevo() {
+  this.router.navigate(['add']);
+  }
+
+  Listar() {
+    this.router.navigate(['listar']);
+  }
 }
